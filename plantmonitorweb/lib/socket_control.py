91bcca8c9080fbd4f4c2e74f14f3839fc1e7b868
Can Eldem <celdem@zendesk.com>
2017-12-02 23:11:20 +0000

    update git ignore

---
+++ plantmonitorweb/lib/socket_control.py
@@ -0,0 +1,26 @@
+# from energenie import switch_on, switch_off
+import sys
+
+
+def switch_on(socket):
+    print('Switching on', socket)
+    return True
+
+
+def switch_off(socket):
+    print('Switching off',  socket)
+    return True
+
+
+def main():
+    if (len(sys.argv) >= 3):
+        action = sys.argv[1].lower()
+        socket = int(sys.argv[2])
+        if (action == 'on'): switch_on(socket)
+        if (action == 'off'): switch_off(socket)
+    else:
+        print('Wrong number of arguments')
+
+
+if __name__ == "__main__":
+    main()
---
+++ plantmonitorweb/lib/tasks/socket_control.rake
@@ -0,0 +1,22 @@
+namespace :socket_control do
+  desc 'Controlling electric sockets for lights'
+  task :socket_control do
+    ARGV.each { |a| task a.to_sym do ; end }
+    action = ARGV[1]
+    socket_number = ARGV[2]
+    system "python #{Rails.root}/lib/socket_control.py #{action} #{socket_number}"
+  end
+
+end
+
+namespace :shell do
+  desc 'Updates crontable for socket tasks'
+  task :update do
+    sh 'bundle exec whenever --update-crontab socket control'
+  end
+
+  desc 'test task just says hi'
+  task :hi do
+    p 'App'
+  end
+end
