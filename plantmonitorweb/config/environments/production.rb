29c9abed45f2584b1e62eabb54e5b51845ad7390
Can Eldem <celdem@zendesk.com>
2017-12-26 18:54:44 +0000

    server test and configuration for production system

--- plantmonitorweb/config/environments/production.rb
+++ plantmonitorweb/config/environments/production.rb
@@ -37,12 +37,14 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- plantmonitorweb/lib/socket_control.py
+++ plantmonitorweb/lib/socket_control.py
@@ -2,13 +2,13 @@ from energenie import switch_on, switch_off
 import sys
 
 
-def switch_on(socket):
+def switch_on_socket(socket):
     print('Switching on', socket)
     switch_on(socket)
     return True
 
 
-def switch_off(socket):
+def switch_off_socket(socket):
     print('Switching off', socket)
     switch_off(socket)
     return True
@@ -18,8 +18,10 @@ def main():
     if (len(sys.argv) >= 3):
         action = sys.argv[1].lower()
         socket = int(sys.argv[2])
-        if (action == 'on'): switch_on(socket)
-        if (action == 'off'): switch_off(socket)
+        if (action == 'on'):
+           switch_on_socket(socket)
+        if (action == 'off'):
+            switch_off_socket(socket)
     else:
         print('Wrong number of arguments')
 
